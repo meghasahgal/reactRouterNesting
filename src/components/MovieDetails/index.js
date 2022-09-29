d278636fbc4b721c2f7dfabe20ca0e2554c74eb8
Megha Sahgal <meghas@gmail.com>
2022-09-29 07:10:11 +0000

    finished!

--- src/components/MovieDetails/index.js
+++ src/components/MovieDetails/index.js
@@ -1,7 +1,14 @@
-function MovieDetails() {
+import { useParams } from "react-router-dom";
+
+function MovieDetails({ movies }) {
+  const { movieId } = useParams();
+  //   console.log(movieId);
+  const movieChoice = movies.find((el) => el.id === parseInt(movieId));
+  //   console.log(movieChoice); returns an obj, so destructure below
   return (
-    <div className='comp purple'>
-      <h1>MovieDetails Component</h1>
+    <div className="comp purple">
+      <h1>{movieChoice.title}</h1>
+      <p>{movieChoice.description}</p>
     </div>
   );
 }
